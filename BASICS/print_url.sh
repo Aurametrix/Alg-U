37ed5a108bfa134ec3b6c3b1cf912b88083fc1f0
Aurametrix <irene@aurametrix.com>
2016-04-01 20:39:47 -0700

    HTTPS URL's content

--- BASICS/print_url.sh
+++ BASICS/print_url.sh
@@ -2,3 +2,8 @@ curl -s -L http://rosettacode.org/
 lynx -source http://rosettacode.org/
 wget -O - -q http://rosettacode.org/
 lftp -c "cat http://rosettacode.org/"
+
+
+
+
+curl -k -s -L https://sourceforge.net/
