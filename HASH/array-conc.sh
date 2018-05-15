9a0fcc2208318f45208ffce02314d5239bc0fa81
Aurametrix <irene@aurametrix.com>
2018-05-15 08:05:21 -0700

    use real arrays not whitespace separated pseudo-array strings
    https://github.com/anordal/shellharden/blob/master/how_to_do_things_safely_in_bash.md
    Safe ways to do things in bash 
    https://news.ycombinator.com/item?id=17057596

--- HASH/array-conc.sh
+++ HASH/array-conc.sh
@@ -3,3 +3,12 @@ y=(5 6)
 sum=( "${x[@]}" "${y[@]}" )
  
 for i in "${sum[@]}" ; do echo "$i" ; done
+
+array=(
+    a
+    b
+)
+array+=(c)
+if [ ${#array[@]} -gt 0 ]; then
+    rm -- "${array[@]}"
+fi
