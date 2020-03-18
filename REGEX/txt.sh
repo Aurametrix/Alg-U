82460d356934bdfebaf1e5f2a3bac81354c9032b
Aurametrix <irene@aurametrix.com>
2020-03-17 21:11:17 -0400

    text processing
    https://blog.balthazar-rouberol.com/text-processing-in-the-shell

--- REGEX/txt.sh
+++ REGEX/txt.sh
@@ -8,3 +8,29 @@ done
 for f in hw1.*; do 
     mv $f ${f//hw1/hw2}; 
 done
+
+
+#####################3
+head -n 2 file.csv
+tail -n 1 file.csv
+tail -n 42 file.csv
+wc -l
+wc - w
+
+grep mutex file.csv | grep OS
+
+
+
+cat
+head
+tail
+wc
+grep
+cut
+paste
+sort
+uniq
+awk
+tr
+fold
+sed
